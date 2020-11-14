ea950b3d2943ebab73bf4ed8d436eb2a399b5070
unknown <richardcarmenscott@gmail.com>
2020-11-14 17:30:05 -0500

    init

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react'
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import React, { Component } from 'react';
+
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import './RegistrationRoute.css';
 
 class RegistrationRoute extends Component {
   static defaultProps = {
     history: {
       push: () => {},
     },
-  }
+  };
 
   handleRegistrationSuccess = () => {
-    const { history } = this.props
-    history.push('/login')
-  }
+    const { history } = this.props;
+    history.push('/login');
+  };
 
   render() {
     return (
-      <section>
+      <section className="reg-log-form">
         <p>
-          Practice learning a language with the spaced reptition revision technique.
+          Practice learning a language with the spaced reptition revision
+          technique.
         </p>
         <h2>Sign up</h2>
         <RegistrationForm
@@ -28,4 +31,4 @@ class RegistrationRoute extends Component {
   }
 }
 
-export default RegistrationRoute
+export default RegistrationRoute;
